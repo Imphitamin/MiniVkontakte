fba696eb970c1f86cfd3936bf02244fa43cf54bf
Dima <ImphitaminXenon@gmail.com>
2015-08-20 20:23:00 +0300

    Creating second screen of app called "LoginActivity"

---
+++ app/src/main/java/com/example/dmitry/minivkontakte/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.dmitry.minivkontakte;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dmitry on 20.08.2015.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+String haa;
+
+}
